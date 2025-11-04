b910b5a023926d4756015b5239dd036626a6a167
Demian Tavolaro <demiantavolaro6@gmail.com>
2025-11-05 00:03:25 +0100

    Exercise 8.17 completed

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -9,26 +9,29 @@ const Books = () => {
   if (loading) return <p>Loading...</p>;
 
   return (
-    <div>
-      <h2>books</h2>
+    console.log(books),
+    (
+      <div>
+        <h2>books</h2>
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>author</th>
+              <th>published</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+            {books.map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
   );
 };
 
